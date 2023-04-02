1eab8743aa699b4d2b7b38752d67b3eebf038a32
GauravRteli <gauravteli134@gmail.com>
2023-04-02 20:58:28 +0530

    bio field added

--- model/AppUser.js
+++ model/AppUser.js
@@ -1,16 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const AppUserSchema = new Schema({
-    name: {type: String,required: true},
-    sex:{type: String,required:true},
-    email: {type: String,required: true},
-    phoneNo: {type: String,required: true},
-    dateOfBirth: {type: String,required: true},
-    skills:{type: Array,required: true},
-    appliedjob:{type: Array,required: false,default: new Array()},
-    recruitedInJob: {type: Object, required: false,default: null}, // job in which the user is working .
-    password: {type: String,required: true},
-},{ timestamps: true })
+const AppUserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    sex: { type: String, required: true },
+    email: { type: String, required: true },
+    phoneNo: { type: String, required: true },
+    dateOfBirth: { type: Date, required: true },
+    skills: { type: Array, required: true },
+    bio: { type: String, required: false, default: "" },
+    appliedjob: { type: Array, required: false, default: new Array() },
+    recruitedInJob: { type: Object, required: false, default: null }, // job in which the user is working .
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('AppUserInfo',AppUserSchema);
+module.exports = mongoose.model("AppUserInfo", AppUserSchema);
--- route/user.js
+++ route/user.js
@@ -7,7 +7,7 @@ const RecruitmentSchema = require("../model/Recruitment.js");
 const AppUserSchema = require("../model/AppUser");
 
 router.post("/appuserregistration", async (req, res) => {
-  const { name, sex,email, phoneNo, dateOfBirth, skills, password } = req.body;
+  const { name, sex, email, phoneNo, dateOfBirth, skills, password } = req.body;
   const user1 = await AppUserSchema.findOne({ email: email });
 
   if (user1) {
@@ -33,23 +33,51 @@ router.post("/appuserregistration", async (req, res) => {
 router.post("/appuser-login", async (req, res) => {
   const { email, password } = req.body;
   const user = await AppUserSchema.findOne({ email: email });
-  if (!user) {
-    res.status(201).send({ message: "Invalid Inputs Username and Password" });
-  } else if (user && !(await bcrypt.compare(password, user.password))) {
-    res.status(201).send({ message: "Invalid Inputs Username and Password" });
-  } else {
-    res.status(200).send(user);
+  try{
+    
+    if (!user) {
+      res.status(201).send({ message: "Invalid Inputs Username and Password" });
+    } else if (user && !(await bcrypt.compare(password, user.password))) {
+      res.status(201).send({ message: "Invalid Inputs Username and Password" });
+    } else {
+      res.status(200).send(user);
+    }
+  }catch(e){
+    res.send(e);
   }
+});
+
+// post -Edit App User
+
+router.post("/edit-appuser", async (req, res) => {
+  const { workerId, name, sex, email, phoneNo, dateOfBirth, skills, bio } = req.body;
+
+  const updated_user = await AppUserSchema.findByIdAndUpdate(workerId,{
+    $set:{
+      name: name,
+      sex: sex,
+      email: email,
+      phoneNo: phoneNo,
+      dateOfBirth: dateOfBirth,
+      skills: skills,
+      bio: bio
+    }
+  },{
+    new: true
+  });
+
+  res.status(200).send(updated_user);
+
 });
 
 // getUser
 
-router.post("/get-appuser",async (req,res) => {
-    console.log(req.body);
-    const user = await AppUserSchema.findById(req.body.userid);
-    console.log(user);
-    res.status(200).send(user);
-})
+router.post("/get-appuser", async (req, res) => {
+  console.log(req.body);
+  const user = await AppUserSchema.findById(req.body.userid);
+  console.log(user);
+  res.status(200).send(user);
+});
 
 // getJobs
 
@@ -60,7 +88,6 @@ router.get("/get-jobs", async (req, res) => {
 
 // registration in job
 router.post("/user-jobapplication", async (req, res) => {
-
   const { user, job_id } = req.body;
   const job = await RecruitmentSchema.findById(job_id);
   let userapplications = job.workerregistered; // the user which have registered in the job
@@ -70,18 +97,21 @@ router.post("/user-jobapplication", async (req, res) => {
 
   jobapplications.push({
     job: job,
-    status: 0
+    status: 0,
   });
   userapplications.push(user);
 
   await RecruitmentSchema.findByIdAndUpdate(job_id, {
-    $set: { workerregistered: userapplications},
+    $set: { workerregistered: userapplications },
   });
-  await AppUserSchema.findByIdAndUpdate(user._id,{
-    $set: { appliedjob: jobapplications }
-  })
-  res.status(200).send({message: `SuccessFully Applied in the job application for ${job.jobname}`});
-
+  await AppUserSchema.findByIdAndUpdate(user._id, {
+    $set: { appliedjob: jobapplications },
+  });
+  res
+    .status(200)
+    .send({
+      message: `SuccessFully Applied in the job application for ${job.jobname}`,
+    });
 });
 
 module.exports = router;
